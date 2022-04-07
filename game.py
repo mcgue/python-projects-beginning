0ee5ae47e479efa42a427c169e8f840ea3d9d7b2
mcgue <mcgu@protonmail.com>
2022-04-07 15:01:30 -0700

    Update game.py

--- game.py
+++ game.py
@@ -19,13 +19,26 @@ print('Well, ' + player_name + ', I am thinking of a number between 1 and 20.')
 
 # start while loop to give player 6 chances to guess the number
 while num_guesses < 6:
-
-    print('Take a guess.')  # ask for and collect input
+    # ask for and collect input
+    print('Take a guess.')
     guess = input()
+    # Verify guess is an integer number between 1 and 20
+    try:
+        int(guess)
+        is_valid = True
+    except ValueError:
+        is_valid = False
+    while is_valid == False:
+        print('Please enter an integer between 1 and 20')
+        guess = input()
+        try:
+            int(guess)
+            is_valid = True
+        except ValueError:
+            is_valid = False
+    # convert guess to integer
     guess = int(guess)
-
     num_guesses = num_guesses + 1 # advance number of guesses
-
     # respond to user input if guess is incorrect or break from loop
     if guess < number:
         print('Your guess is too low.')
